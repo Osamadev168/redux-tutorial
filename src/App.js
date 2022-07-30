3ad8b25eec76c457e72e0d5d47e573813087e8ab
osamadev168 <osamatwenty@gmail.com>
2022-07-30 23:33:55 +0500

    dispatched actions

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { increment, decrement } from './actions';
 function App() {
   const counter = useSelector(state => state.counter)
-
-
+  const auth = useSelector(state => state.auth)
+  const dispatch = useDispatch()
   return (
     <div className="App">
       <h1>Hello Jigger!</h1>
-      <h1>Counter {counter}</h1>
+      {auth ? <h1>Hello again</h1> : null}
+
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+
+      <h1>Counter :  {counter}</h1>
+
     </div>
   );
 }
